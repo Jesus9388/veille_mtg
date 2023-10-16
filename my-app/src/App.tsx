57258e6d9dbdd1534662daf660ed339d3e8b578a
Jesus9388 <joshua_938@outlook.com>
2023-10-16 13:41:17 -0400

    It no work

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import * as Scry from "scryfall-sdk";
 import "./App.css";
 
 function App() {
+
+  const asyncShit = async () => {
+    const chalice = await Scry.Cards.byName("Chalice of the Void");
+    console.log(chalice);
+  }
+
+  useEffect(() => {
+    asyncShit();
+  }, []);
+
   return (
     <>
       <h1 className="text-3xl font-bold underline">WE ARE REACTING</h1>
