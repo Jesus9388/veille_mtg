ed541c334d4935c8c268e8b01e716ddc4ea2add2
Cédric <2212895@claurendeau.qc.ca>
2023-10-23 12:10:00 -0400

    nuke

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,21 +1,11 @@
-import React, { useEffect } from "react";
-import * as Scry from "scryfall-sdk";
 import "./App.css";
+import TextReader from "./components/TextReader";
 
 function App() {
-
-  const asyncShit = async () => {
-    const chalice = await Scry.Cards.byName("Chalice of the Void");
-    console.log(chalice);
-  }
-
-  useEffect(() => {
-    asyncShit();
-  }, []);
-
   return (
     <>
       <h1 className="text-3xl font-bold underline">WE ARE REACTING</h1>
+      <TextReader></TextReader>
     </>
   );
 }
