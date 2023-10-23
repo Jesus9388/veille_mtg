462e2133a78136735707dcdac69b3504cb11159c
Cédric <2212895@claurendeau.qc.ca>
2023-10-23 12:56:29 -0400

    To make good commit message or not to make good commit message, that is the question

--- my-app/src/components/TextReader/index.tsx
+++ my-app/src/components/TextReader/index.tsx
@@ -1,12 +1,31 @@
 interface Props {
-    setDeckList: (text: string) => void;
+    setDeckList: (text: string[]) => void;
 }
 
 const TextReader = ({setDeckList} : Props) => {
+    let unformattedDeckList : string = "";
+
+    const formatDeckList = () => {
+        let listNames = unformattedDeckList.split("\n");
+        let formattedDeckList = [];
+        for (let i = 0; i < listNames.length; i++) {
+            let splitCardName = listNames[i].split(" ");
+            let cardName = "";
+            for (let j = 1; j < splitCardName.length; j++) {
+                cardName = cardName + splitCardName[j] + " ";
+            }
+            cardName = cardName.trim();
+            formattedDeckList.push(cardName);
+        }
+        console.log(formattedDeckList);
+    }
+
     return (
         <div className="container content-center">
-            <textarea placeholder="Please input your deck list in MTGA format" className="min-h-[200px] w-full rounded bg-white border-2 border-black"></textarea>
-            <button className="w-full text-center border-black border bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Submit</button>
+            <textarea onChange={(event) => {
+                unformattedDeckList = event.target.value;
+            }} placeholder="Please input your deck list in MTGO format" className="min-h-[200px] w-full rounded bg-white border-2 border-black"></textarea>
+            <button onClick={formatDeckList} className="w-full text-center border-black border bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Submit</button>
         </div>
     );
 };
