d821881c3235ea9050323186b94392f3f7a86154
Cédric <2212895@claurendeau.qc.ca>
2023-10-23 12:27:21 -0400

    haha

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import TextReader from "./components/TextReader";
 
+const setText = (text: string) => {
+  console.log(text);
+}
+
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline text-center">WE ARE GATHERING</h1>
-      <TextReader></TextReader>
+      <TextReader setText={setText}></TextReader>
     </>
   );
 }
