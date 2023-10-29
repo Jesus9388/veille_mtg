d93cdf8e8476c981472f251171b9caf2d4b11269
joshua kuzak <joshua_938@outlook.com>
2023-10-29 14:50:51 -0400

    Added cool useless ping animation

---
+++ my-app/src/components/IllegalCards/IllegalCard/index.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { Card } from "../../../model/magic";
+
+interface Props {
+  illegalCard: Card;
+}
+
+const IllegalCard = ({ illegalCard }: Props) => {
+  const [hasBeenHovered, setHasBeenHovered] = useState<boolean>(false);
+
+  const onHover = () => {
+    setHasBeenHovered(true);
+  }
+
+  return (
+    <div className="relative" onMouseEnter={onHover}>
+      {!hasBeenHovered && (
+        <span className="absolute flex h-3 w-3 right-0 top-0">
+          <span className="animate-ping h-full w-full absolute inline-flex rounded-full bg-sky-400 opacity-75"></span>
+          <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+        </span>
+      )}
+      <div className="border-dashed border-2 text-center mb-2 p-3 border-red-500 border-opacity-50 hover:border-opacity-100 hover:bg-red-500 hover:text-white rounded-lg">
+        {illegalCard.name}
+      </div>
+    </div>
+  );
+};
+
+export default IllegalCard;
--- my-app/src/components/IllegalCards/index.tsx
+++ my-app/src/components/IllegalCards/index.tsx
@@ -1,4 +1,5 @@
 import { Card } from "../../model/magic";
+import IllegalCard from "./IllegalCard";
 
 interface Props {
   illegalCards: Card[];
@@ -9,22 +10,15 @@ const IllegalCards = ({ illegalCards, hasBeenSubmitted }: Props) => {
   if (!hasBeenSubmitted) return <></>;
 
   return (
-    <>
+    <div className="mt-3">
       {illegalCards.length === 0 ? (
         <div className="text-center">No illegal cards!</div>
       ) : (
         illegalCards.map((card) => {
-          return (
-            <div
-              className="border-dashed border-2 text-center my-2 p-3 border-red-500 border-opacity-50 hover:border-opacity-100 hover:bg-red-500 hover:text-white rounded-lg"
-              key={card.name}
-            >
-              {card.name}
-            </div>
-          );
+          return <IllegalCard illegalCard={card} key={card.name} />;
         })
       )}
-    </>
+    </div>
   );
 };
 
