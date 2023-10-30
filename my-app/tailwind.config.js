b4301b996327d741fdfce6f4df8bdaf3b4f30cf3
Cédric <2212895@claurendeau.qc.ca>
2023-10-30 12:44:22 -0400

    ye

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -7,7 +7,17 @@ module.exports = {
     container: {
       center: true,
     },
-    extend: {},
+    extend: {
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',},
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(0deg)' },
+          '25%, 75%': { transform: 'rotate(-90deg)'},
+          '50%': { transform: 'rotate(90deg)' },
+        },
+      },
+    },
   },
   plugins: [],
 }
